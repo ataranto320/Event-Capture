281fccae3db48a5b473ebcce0711cba4a356e44d
Austin T <austin.taranto@gmail.com>
2020-03-20 15:18:16 -0400

    using once with button

--- javascript.js
+++ javascript.js
@@ -1,16 +1,24 @@
 const divs = document.querySelectorAll("div");
+const button = document.querySelector("button");
 
 function logText(e) {
     console.log(this.classList.value);
     // console.log(this);
-    // stop bubbling this event up
-    e.stopPropagation();
+    // e.stopPropagation(); // stop bubbling this event up
 }
 
 // document.body.addEventListener("click", logText);
 
 divs.forEach(div => div.addEventListener("click", logText, {
-    // run function on way down - capture true
-    // capture: true
-    capture: false
+    // capture: true // run function on way down - capture true
+    capture: false,
+    once: true // unbind itself // wont run again
 }));
+
+button.addEventListener("click", () => {
+    console.log("click.");
+}, {
+    once: true
+});
+
+// once similar to: div.removeEventListener("click", logText);
